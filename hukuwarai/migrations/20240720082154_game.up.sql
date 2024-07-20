492d11e779ed17cb736b34532a2f507782072726
Mutsuha Asada <me@momee.mt>
2024-07-21 00:35:14 +0900

    fix: missing field (answer_url)

--- hukuwarai/migrations/20240720082154_game.up.sql
+++ hukuwarai/migrations/20240720082154_game.up.sql
@@ -3,5 +3,6 @@ create table games (
     id int primary key,
     name text not null,
     started boolean not null default false,
-    finished boolean not null default false 
+    finished boolean not null default false,
+    answer_url text not null,
 );
