1050703e1d61a22d9e71c74129f48624359dcc35
Shina <53410646+s7tya@users.noreply.github.com>
2024-07-21 02:06:03 +0900

    impl get players

--- hukuwarai/src/model/player.rs
+++ hukuwarai/src/model/player.rs
@@ -2,13 +2,35 @@
 pub struct Player {
     pub id: i32,
     pub name: String,
-    pub game_id: i64,
+    pub game_id: i32,
+}
+
+impl From<Player> for openapi::models::Player {
+    fn from(player: Player) -> Self {
+        openapi::models::Player {
+            id: Some(player.id),
+            name: Some(player.name),
+            game_id: Some(player.game_id),
+        }
+    }
 }
 
 pub async fn get_all_players(pool: &sqlx::PgPool) -> Result<Vec<Player>, sqlx::Error> {
     let players = sqlx::query_as::<_, Player>("SELECT id, name, game_id FROM players")
         .fetch_all(pool)
         .await?;
+    Ok(players)
+}
+
+pub async fn get_all_players_from_game_id(
+    pool: &sqlx::PgPool,
+    game_id: i32,
+) -> Result<Vec<Player>, sqlx::Error> {
+    let players =
+        sqlx::query_as::<_, Player>("SELECT id, name, game_id FROM players WHERE game_id = $1")
+            .bind(game_id)
+            .fetch_all(pool)
+            .await?;
     Ok(players)
 }
 
