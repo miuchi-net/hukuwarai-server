da07fd69ddd1c3d07ad1d9f93ab3be0ee9f23cc2
Mutsuha Asada <me@momee.mt>
2024-07-21 09:50:36 +0900

    add: scores API template

--- hukuwarai/src/handlers/scores.rs
+++ hukuwarai/src/handlers/scores.rs
@@ -1,8 +1,10 @@
 use axum::{async_trait, extract::Host, http::Method};
 use axum_extra::extract::CookieJar;
 use openapi::{
-    apis::scores::{PostScoresResponse, Scores},
-    models::{PostScoresPathParams, PostScoresRequest},
+    apis::scores::{GetScoresResponse, GetScoresResultResponse, PostScoresResponse, Scores},
+    models::{
+        GetScoresPathParams, GetScoresResultPathParams, PostScoresPathParams, PostScoresRequest,
+    },
 };
 
 use super::api_impl::ApiImpl;
@@ -17,6 +19,26 @@ impl Scores for ApiImpl {
         _path_params: PostScoresPathParams,
         _body: Option<PostScoresRequest>,
     ) -> Result<PostScoresResponse, String> {
+        todo!()
+    }
+
+    async fn get_scores_result(
+        &self,
+        _method: Method,
+        _host: Host,
+        _cookies: CookieJar,
+        _path_params: GetScoresResultPathParams,
+    ) -> Result<GetScoresResultResponse, String> {
+        todo!()
+    }
+
+    async fn get_scores(
+        &self,
+        _method: Method,
+        _host: Host,
+        _cookies: CookieJar,
+        _path_params: GetScoresPathParams,
+    ) -> Result<GetScoresResponse, String> {
         todo!()
     }
 }
